4098d50f44d7ffaacb372dc6c68910f0e4028841
Tom Bayne <tombayne01@gmail.com>
2021-08-29 19:36:44 +0000

    Added Steam TV Mode Script

---
+++ tv_steam.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ARG=$1
+
+#turn on tv output, set it as left and primary display, Change output and mode as required.
+xrandr --output 'HDMI-0' --mode '3840x2160' --left-of 'DP-2' --primary
+
+#move all windows right, move by X resolution.
+output=$(/usr/bin/wmctrl -lG)
+while read -r line; do
+    $(echo $line | awk '{print "/usr/bin/wmctrl -i -r " $1 " -e 0," $3+3840 ",-1,-1,-1;" }')
+done <<< "$output"
+
+sleep 2
+
+if [ "$ARG" == "wine" ]
+then
+	echo "running wine version"
+	wine /crypt/wine/drive_c/Program\ Files/Steam/Steam.exe -no-cef-sandbox
+else
+	/usr/bin/steam-native -bigpicture
+fi
+
+#undo the above when steam closes.
+
+#move all windows left
+output=$(/usr/bin/wmctrl -lG)
+while read -r line; do
+    $(echo $line | awk '{print "/usr/bin/wmctrl -i -r " $1 " -e 0," $3-3840 ",-1,-1,-1;" }')
+done <<< "$output"
+
+xrandr --output 'DP-3' --primary --output 'HDMI-0' --off 
+
