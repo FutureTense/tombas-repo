43f6c207dd735ba3a62d7556ff0dc524a555a7a6
FutureTense <FutureTense@users.noreply.github.com>
2022-03-31 20:58:41 +0000

    restore

--- getcities.sh
+++ getcities.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 country=$1
-country="united_states"
 output=$(nordvpn cities $country)
 upd="A new version of NordVPN is available! Please update the application."
 echo "COUNTRY $country"
 
 cities=$output
 #cities=$(printf '%s' "${output//$upd/}")
-#cities="Atlanta, Buffalo, Charlotte, Chicago, Dallas, Denver, Kansas_City, Los_Angeles, Manassas, Miami, New_York, Phoenix, Saint_Louis, Salt_Lake_City, San_Francisco, Seattle"
 
 myarray=(`echo $cities | tr ',' ' '`)
 
